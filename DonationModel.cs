60d8a0a8f30a612986a5ddb631af7981529966cf
Ayesha-98 <145263106+Ayesha-98@users.noreply.github.com>
2025-07-04 00:00:25 +0500

    Entities_Layer_Files

---
+++ DonationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+        public class DonationModel
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public int DonorId { get; set; }
+
+            [Required]
+            public DateTime DonationDate { get; set; }
+
+            public int? VolumeML { get; set; }
+
+            public int? StaffId { get; set; }
+
+            public string? Remarks { get; set; }
+        }
+    }
+

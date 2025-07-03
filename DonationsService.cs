0e808da826d482c17e16bd1e7e2bedcd36a2d388
Ayesha-98 <145263106+Ayesha-98@users.noreply.github.com>
2025-07-04 00:01:16 +0500

    Business-Logic_Layer_Files

---
+++ DonationsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class DonationService
+    {
+        private readonly Donations _donationRepo;
+
+        public DonationService()
+        {
+            _donationRepo = new Donations();
+        }
+
+        public List<DonationModel> GetAllDonations()
+        {
+            return _donationRepo.GetAllDonations();
+        }
+
+        public void AddDonation(DonationModel donation)
+        {
+            _donationRepo.AddDonation(donation);
+        }
+
+        public void UpdateDonation(DonationModel donation)
+        {
+            _donationRepo.UpdateDonation(donation);
+        }
+
+        public void DeleteDonation(int id)
+        {
+            _donationRepo.DeleteDonation(id);
+        }
+    }
+}
---
+++ UserService.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class UserService
+    {
+        private readonly UserRepository _repo;
+
+        public UserService(UserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void CreateUserIfNotExists(UserModel user)
+        {
+            if (string.IsNullOrWhiteSpace(user.Email))
+                return; // or throw an exception if email is required
+
+            var existingUser = _repo.GetUserByEmail(user.Email);
+            if (existingUser == null)
+            {
+                _repo.AddUser(user);
+            }
+        }
+    }
+}
